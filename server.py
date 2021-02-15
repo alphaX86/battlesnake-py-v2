f2ea4bf57102b96d4343a74b97472cd3a43b5204
Aadhitya A <aadhitya864@gmail.com>
2021-02-15 14:52:16 +0530

    Customize battlesnake
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#888888",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "author": "alphaX86",  # TODO: Your Battlesnake Username
+            "color": "#000000",  # TODO: Personalize
+            "head": "gamer",  # TODO: Personalize
+            "tail": "leaf",  # TODO: Personalize
         }
 
     @cherrypy.expose
